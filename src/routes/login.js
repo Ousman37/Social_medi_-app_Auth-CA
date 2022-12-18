f1cb65fd57018cdc05691ef35ba29c851080a97b
Ethmane Didi Bilal <Ethmane.Bilal@stud.noroff.no>
2022-12-18 12:18:05 +0100

    Implementing
    working on loading

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,63 +1,53 @@
 import { loginUrl } from './url.js';
 
-let loader = document.getElementById('loading');
-function validateForm() {
-    loader.classList.remove('display-none');
-    // Form validation code goes here
-    
+const loadingElement = document.getElementById('loading');
+
+function showLoading() {
+    loadingElement.style.display = 'block';
+}
+
+function hideLoading() {
+    loadingElement.style.display = 'none';
+}
+
+showLoading();
+
+setTimeout(hideLoading, 5000);
+
+async function validateForm(event) {
+    event.preventDefault(); // prevent the form from being submitted
+
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    let userBody = {
-        email: `${email}`,
-        password: `${password}`,
+    const userBody = {
+        email,
+        password,
     };
 
-    //POST /api/v1/social/auth/login
     const opts = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(userBody),
     };
 
-    // eslint-disable-next-line no-unused-vars
-    let flashMessage = '';
+    try {
+        showLoading();
+        const res = await fetch(loginUrl, opts);
+        const resp = await res.json();
 
-    fetch(loginUrl, opts)
-        .then(res => res.json())
-        .then(resp => {
-            loader.classList.add('display-none');
-
-            if (resp.accessToken) {
-                localStorage.setItem('token', `${resp.accessToken}`);
-                window.location = '/';
-            } else {
-                document.getElementById('login-error').innerHTML =
-          resp.errors[0].message;
-            }
-        });
-
-    console.log(validateForm);
-    // Email validation
-    const emailRegex = /^[A-Za-z0-9._+-]+@[A-Za-z0-9.-]+\.[a-z]{2,4}/;
-    if (!emailRegex.test(email.value)) {
-        alert('Please enter a valid email address');
-        return false;
+        if (resp.accessToken) {
+            localStorage.setItem('token', resp.accessToken);
+            window.location = '/';
+        } else {
+            document.getElementById('login-error').innerHTML = resp.errors[0].message;
+        }
+    } catch (error) {
+        console.error(error);
+    } finally {
+        hideLoading();
     }
-
-    // Password validation
-    const passwordRegex =
-    // eslint-disable-next-line no-useless-escape
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
-    if (!passwordRegex.test(password.value)) {
-        alert('Please enter a valid password');
-        return false;
-    }
-    return true;
 }
-
-// let email = document.getElementById('email').value;
-// let password = document.getElementById('password').value;
 
 const form = document.getElementById('form');
 form.addEventListener('submit', validateForm);
--- src/routes/test.js
+++ src/routes/test.js
@@ -41,3 +41,69 @@ document
 
 
     
+
+
+import { loginUrl } from './url.js';
+
+const loader = document.getElementById('loading');
+
+function validateForm() {
+    //loader.classList.remove('display-none');
+    // Form validation code goes here
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const userBody = {
+        email: `${email}`,
+        password: `${password}`,
+    };
+
+    //POST /api/v1/social/auth/login
+    const opts = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(userBody),
+    };
+
+    // eslint-disable-next-line no-unused-vars
+    const flashMessage = '';
+
+    fetch(loginUrl, opts)
+        .then(res => res.json())
+        .then(resp => {
+            loader.classList.add('display-none');
+
+            if (resp.accessToken) {
+                localStorage.setItem('token', `${resp.accessToken}`);
+                window.location = '/';
+            } else {
+                document.getElementById('login-error').innerHTML =
+              resp.errors[0].message;
+            }
+        });
+
+    console.log(validateForm);
+    // Email validation
+    const emailRegex = /^[A-Za-z0-9._+-]+@[A-Za-z0-9.-]+\.[a-z]{2,4}/;
+    if (!emailRegex.test(email.value)) {
+        alert('Please enter a valid email address');
+        return false;
+    }
+
+    // Password validation
+    const passwordRegex =
+        // eslint-disable-next-line no-useless-escape
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+    if (!passwordRegex.test(password.value)) {
+        alert('Please enter a valid password');
+        return false;
+    }
+    return true;
+}
+
+// let email = document.getElementById('email').value;
+// let password = document.getElementById('password').value;
+
+const form = document.getElementById('form');
+form.addEventListener('submit', validateForm);
